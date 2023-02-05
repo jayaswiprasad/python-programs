0f7a7d989ff3ad429a10b3848511e221b745607e
Masuna Jayaswi prasad <109474947+jayaswiprasad@users.noreply.github.com>
2023-02-05 23:09:59 +0530

    Delete cyberpy.py

--- cyberpy.py
+++
@@ -1,7 +0,0 @@
-import collections
-import pprint
-file_input=input("mooc.txt")
-with open(file_input,'r') as info:
-    count=collections.Counter(info.read().upper())
-    value=pprint.pformat(count)
-print(value)
